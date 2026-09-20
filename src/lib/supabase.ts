e6402c186fbdc5f99cc451e8b41550253846975d
HarishY <hyarlagadda23@gmail.com>
2026-09-20 06:41:51 -0400

    supbase config changes

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,6 +2,7 @@ import { createClient } from "@supabase/supabase-js"
 import { projectId, publicAnonKey } from "../../utils/supabase/info"
 
 export const supabase = createClient(
-  `https://${projectId}.supabase.co`,
-  publicAnonKey,
+  `https://${import.meta.env.VITE_SUPABASE_PROJECT_ID || projectId
+  }.supabase.co`,
+  import.meta.env.VITE_SUPABASE_PUBLIC_ANON_KEY || publicAnonKey,
 )
